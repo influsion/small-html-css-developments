49d60cca7b2bf2e713e23655c148edd525fb97bd
Alexander Barna <influsion@gmail.com>
2021-07-14 00:54:35 +0300

    build configuration

---
+++ gulpfile.esm.js
@@ -0,0 +1,55 @@
+import {
+  src,
+  dest,
+  watch,
+  series,
+  parallel,
+} from 'gulp';
+import rename from 'gulp-rename';
+import del from 'del';
+
+const sass = require('gulp-sass')(require('sass'));
+
+export function clean() {
+  return del([
+    'dist',
+  ]);
+}
+
+export function scss() {
+  return src('./src/scss/main.scss', {
+      allowEmpty: true,
+    })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('styles.css'))
+    .pipe(dest('./dist/css'));
+}
+
+export function html() {
+  return src('./src/html/**/*.html')
+    .pipe(dest('./dist'));
+}
+
+export function images() {
+  return src('./src/images/**/*.+(jpg|png|svg|gif)')
+    .pipe(dest('./dist/images'));
+}
+
+export function test(cb) {
+  console.log('GULP TEST');
+  cb();
+}
+
+export const build = series(clean, parallel(scss, html, images));
+
+export const watching = function() {
+  watch([
+    './src/scss/**/*.scss',
+    './src/html/**/*.html',
+    './src/images/**/*.+(jpg|png|svg|gif)',
+  ], build);
+};
+
+export const dev = parallel(watching, build);
+
+export default test;
